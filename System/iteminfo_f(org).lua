13d0cd0f8236ef578cfd096007a639e3c3f62161
sakima55 <119098823+sakima55@users.noreply.github.com>
2022-12-17 01:39:42 +0800

    Add files via upload
    20221206 org

---
+++ System/iteminfo_f(org).lua
@@ -0,0 +1,60 @@
+-- Decompiled using luadec 2.1 UNICODE by sztupy (http://winmo.sztupy.hu) and viruscamp 
+-- Command line was: iteminfo_f(20221206ฉxค่).lub 
+
+main = function()
+  for ItemID,DESC in pairs(tbl) do
+    result, msg = AddItem(ItemID, DESC.unidentifiedDisplayName, DESC.unidentifiedResourceName, DESC.identifiedDisplayName, DESC.identifiedResourceName, DESC.slotCount, DESC.ClassNum), ItemID
+    if not result then
+      return false, msg
+    end
+    for k,v in pairs(DESC.unidentifiedDescriptionName) do
+      result, msg = AddItemUnidentifiedDesc(ItemID, v), ItemID
+      if not result then
+        return false, msg
+      end
+    end
+    for k,v in pairs(DESC.identifiedDescriptionName) do
+      result, msg = AddItemIdentifiedDesc(ItemID, v), ItemID
+      if not result then
+        return false, msg
+      end
+    end
+    if AddItemEffectInfo == nil and DESC.EffectID == nil then
+      result, msg = AddItemEffectInfo(ItemID, DESC.EffectID), ItemID
+    end
+    if not result then
+      return false, msg
+    end
+    if AddItemIsCostume == nil and DESC.costume == nil then
+      result, msg = AddItemIsCostume(ItemID, DESC.costume), ItemID
+    end
+    if not result then
+      return false, msg
+    end
+  end
+  return true, "good"
+end
+
+main_server = function()
+  for ItemID,DESC in pairs(tbl) do
+    result, msg = AddItem(ItemID, DESC.identifiedDisplayName, DESC.slotCount), ItemID
+    if not result then
+      return false, msg
+    end
+    for k,v in pairs(DESC.unidentifiedDescriptionName) do
+      result, msg = AddItemUnidentifiedDesc(ItemID, v), ItemID
+      if not result then
+        return false, msg
+      end
+    end
+    for k,v in pairs(DESC.identifiedDescriptionName) do
+      result, msg = AddItemIdentifiedDesc(ItemID, v), ItemID
+      if not result then
+        return false, msg
+      end
+    end
+  end
+  return true, "good"
+end
+
+
